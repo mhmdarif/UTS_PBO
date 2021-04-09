e7e1b0b02fb992a1195b1040009763ee6b70d650
MuhammadArifH <arif.chgamer.ad@gmail.com>
2021-04-09 10:06:47 +0700

    menambahkan class pembeli

---
+++ consoleUTS_PBO/Pembeli.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoleUTS_PBO
+{
+    class Pembeli
+    {
+        public string nama_pembeli { get; set; }
+
+        public void lihatSemuaBarang()
+        {
+
+        }
+
+        public void cariBarang()
+        {
+
+        }
+
+        public void beliBarang()
+        {
+
+        }
+        
+        public void masukanKeranjang()
+        {
+
+        }
+        
+    }
+}
---
+++ consoleUTS_PBO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consoleUTS_PBO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
